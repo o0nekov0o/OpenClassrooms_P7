fa1ef99b435d8c972e3c3318c9b7730dc93d5589
o0nekov0o <33200665+o0nekov0o@users.noreply.github.com>
2023-04-13 03:58:29 +0200

    Finalisation du fichier bruteforce.py
    Ajout d'extraits du fichier main.py

--- bruteforce.py
+++ bruteforce.py
@@ -10,18 +10,6 @@ cout_total = 0
 plafond = 500
 
 
-def premiere_methode(plafond, liste_actions, cout_total, benefice_total):
-    actions_triees = sorted(liste_actions, key=lambda x: x[2])
-    actions_selectionnees = []
-    while actions_triees:
-        action = actions_triees.pop()
-        if action[1] + cout_total <= plafond:
-            actions_selectionnees.append(action)
-            benefice_total += action[2]
-            cout_total += action[1]
-    return sum([i[2] for i in actions_selectionnees]), actions_selectionnees
-
-
 def force_brute(plafond, liste_actions, actions_selectionnees):
     if liste_actions:
         val1, lstVal1 = force_brute(plafond, liste_actions[1:], actions_selectionnees)
@@ -35,31 +23,4 @@ def force_brute(plafond, liste_actions, actions_selectionnees):
         return sum([i[2] for i in actions_selectionnees]), actions_selectionnees
 
 
-def sacADos_dynamique(capacite, elements):
-    matrice = [[0 for x in range(capacite + 1)] for x in range(len(elements) + 1)]
-
-    for i in range(1, len(elements) + 1):
-        for w in range(1, capacite + 1):
-            if elements[i-1][1] <= w:
-                matrice[i][w] = max(elements[i-1][2] + matrice[i-1][w-elements[i-1][1]], matrice[i-1][w])
-            else:
-                matrice[i][w] = matrice[i-1][w]
-
-    # Retrouver les éléments en fonction de la somme
-    w = capacite
-    n = len(elements)
-    elements_selection = []
-
-    while w >= 0 and n >= 0:
-        e = elements[n-1]
-        if matrice[n][w] == matrice[n-1][w-e[1]] + e[2]:
-            elements_selection.append(e)
-            w -= e[1]
-
-        n -= 1
-
-    return matrice[-1][-1], elements_selection
-
-
-print('Algo debut', premiere_methode(plafond, liste_actions, cout_total, benefice_total))
 print('Algo force', force_brute(plafond, liste_actions, actions_selectionnees))
