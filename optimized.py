c6b0175c2949f034b932a566bba5b72a46f8988e
o0nekov0o <33200665+o0nekov0o@users.noreply.github.com>
2023-04-13 03:59:53 +0200

    Finalisation du fichier optimized.py
    Ajout d'extraits du fichier main.py (méthode glouton)

--- optimized.py
+++ optimized.py
@@ -5,20 +5,21 @@ liste_actions = [('action_1', 20, 5), ('action_2', 30, 10), ('action_3', 50, 15)
                  ('action_17', 4, 12), ('action_18', 10, 14), ('action_29', 24, 21), ('action_20', 114, 18)]
 actions_triees = sorted(liste_actions, key=lambda x: x[2])
 actions_selectionnees = []
-gain_total = 0
+benefice_total = 0
 cout_total = 0
 plafond = 500
 
-while actions_triees:
-    action = actions_triees.pop()
-    if action[1] + cout_total <= plafond:
-        actions_selectionnees.append(action)
-        gain_total += action[2]
-        cout_total += action[1]
-print("Voici la liste des actions sélectionnées : ")
-for i in range(len(actions_selectionnees)):
-    print(f"{actions_selectionnees[i][0]}, "
-          f"Coût : {actions_selectionnees[i][1]}€, "
-          f"Bénéfice : {actions_selectionnees[i][2]}%")
-benefice_total = gain_total/len(actions_selectionnees)
-print(f"Votre bénéfice total a été amélioré à {benefice_total}% pour un coût total de {cout_total}€")
+
+def premiere_methode(plafond, liste_actions, cout_total, benefice_total):
+    actions_triees = sorted(liste_actions, key=lambda x: x[2])
+    actions_selectionnees = []
+    while actions_triees:
+        action = actions_triees.pop()
+        if action[1] + cout_total <= plafond:
+            actions_selectionnees.append(action)
+            benefice_total += action[2]
+            cout_total += action[1]
+    return sum([i[2] for i in actions_selectionnees]), actions_selectionnees
+
+
+print('Algo debut', premiere_methode(plafond, liste_actions, cout_total, benefice_total))
